124edf6b05fce52210c9a716c345496d3072f8bf
Lasha Kakabadze <lasha@circle.ge>
2020-02-25 18:27:29 +0400

    onclick function will now set cards value on first click, cards with same value will now be  disabled

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment, useEffect, useRef } from "react";
 import { Row } from "antd";
 
 import Card from "../Card";
@@ -12,63 +12,86 @@ const Wrapper = () => {
     value: 0,
     id: 0
   });
+  const buttonClick = useRef();
 
-  const onCardClick = (value, id) => {
-    let newItem = { ...numbers[id], visible: true };
+  useEffect(() => {
+    async function onCardClick(value, id) {
+      let newItem = { ...numbers[id], visible: true, disabled: false };
+      let prevItem = { ...numbers[card.id], visible: true, disabled: false };
 
-    const updatedArray = [
-      ...numbers.slice(0, id),
-      newItem,
-      ...numbers.slice(id + 1)
-    ];
+      const updatedArray = [
+        ...numbers.slice(0, id),
+        newItem,
+        ...numbers.slice(id + 1)
+      ];
 
-    setNumbers(updatedArray);
+      setNumbers(updatedArray);
 
-    if (value.value === card.value.value && !value.disabled) {
-      setCard({
-        value: 0,
-        id: 0
-      });
-    } else
-      setTimeout(() => {
-        newItem = { ...numbers[id], visible: false };
-        let prevArray = { ...numbers[card.id], visible: false };
+      if (card.value === 0 && !value.disabled) {
         setCard({
-          value: value,
+          value: value.value,
           id: id
         });
+      } else if (
+        !value.disabled &&
+        !value.disabled &&
+        value.value !== card.value
+      ) {
+        setTimeout(() => {
+          newItem = { ...newItem, visible: false };
+          prevItem = { ...prevItem, visible: false };
+          setCard({
+            value: 0
+          });
+          let updatedArray = [
+            ...numbers.slice(0, card.id),
+            prevItem,
+            ...numbers.slice(card.id + 1)
+          ];
+
+          setNumbers(updatedArray);
+        }, 300);
+      } else if (value.value === card.value && !value.disabled) {
+        setCard({
+          value: 0
+        });
+        newItem = { ...newItem, disabled: true, visible: true };
+        prevItem = { ...prevItem, disabled: true, visible: true };
         let newArr = [
           ...numbers.slice(0, card.id),
-          prevArray,
+          prevItem,
           ...numbers.slice(card.id + 1)
         ];
 
-        const newArray = [
+        const updatedArray = [
           ...newArr.slice(0, id),
           newItem,
           ...newArr.slice(id + 1)
         ];
-        setNumbers(newArray);
-      }, 300);
-  };
 
-  const cardListing = numbers.map((item, id) => {
-    return (
-      <Card
-        visible={item.visible}
-        number={item.value}
-        onClick={() => onCardClick(item, id)}
-        key={id}
-        disabled={item.disabled}
-      />
-    );
-  });
+        setNumbers(updatedArray);
+      }
+    }
+    buttonClick.current = onCardClick;
+  }, [card, numbers]);
 
   return (
     <Fragment>
       <S.Title>Mahjong-like game</S.Title>
       <Row type="flex" justify="center" align="middle">
-        <S.Wrapper>{cardListing}</S.Wrapper>
+        <S.Wrapper>
+          {numbers.map((item, id) => {
+            return (
+              <Card
+                visible={item.visible}
+                number={item.value}
+                onClick={() => buttonClick.current(item, id)}
+                key={id}
+                disabled={item.disabled}
+              />
+            );
+          })}
+        </S.Wrapper>
       </Row>
     </Fragment>
   );
